56579e6f446d4ff77383ebdea577bea7785609be
MathiasMoelgaard <mathias.molgaard@gmail.com>
2020-05-17 22:35:14 -0700

    Add files via upload

--- src/agent/tcn_agent.py
+++ src/agent/tcn_agent.py
@@ -2,14 +2,11 @@ from trade_platform.src.agent.agent_thread import agent_thread
 from ...src.util.util import *
 import numpy as np
 from ...src.util.mrkt_data import mrkt_data
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense, add, Lambda, GlobalMaxPooling1D, GlobalAveragePooling1D, concatenate
+from tensorflow.keras import Input, Model, backend
 from sklearn import preprocessing
 from ...src.util.Data_parsing.data_parsing import parse
-import keras.backend as K
-from keras.layers import Activation, Lambda
-from keras.layers import Conv1D, SpatialDropout1D
-from keras.layers import Convolution1D, add
+import matplotlib.pyplot as plt
 
 #From https://github.com/philipperemy/keras-tcn
 from tcn import TCN, tcn_full_summary
@@ -19,7 +16,7 @@ class tcn_agent(agent_thread):
     '''
     TCN network takes in the number of moments to look at
     '''
-    def __init__(self, moments = 100, batch_size = None, input_dim = 4 ):
+    def __init__(self, moments = 50, batch_size = None, input_dim = 4 ):
         agent_thread.__init__(self)
         self.moments = moments #Number of moments looked back to decide next value
         self.holding_time = 0
@@ -30,16 +27,19 @@ class tcn_agent(agent_thread):
         self.training = True
         self.networth = 0
         self.amount = 0
+        self.hist_prediction = list()
 
     def _find_decision(self):
         if len(self.market_history) > self.moments:
             predicted_value = self.run_model()
-            print("Open is : ", self.market_history[self.time_counter - 1].open)
-            print("Close is : ", self.market_history[self.time_counter - 1].close)
-            print("Low is : ", self.market_history[self.time_counter - 1].low)
-            print("High is : ", self.market_history[self.time_counter - 1].high)
+            #print("Open is : ", self.market_history[self.time_counter - 1].open)
+            #print("Close is : ", self.market_history[self.time_counter - 1].close)
+            #print("Low is : ", self.market_history[self.time_counter - 1].low)
+            #print("High is : ", self.market_history[self.time_counter - 1].high)
             if not self.holding and (predicted_value[1] - predicted_value[0] > 0 and predicted_value[1] > 0):
-                self.amount = 10
+                self.amount = 100#int(1/predicted_value[3]-predicted_value[2])
+                if self.amount < 0:
+                    self.amount = 1
                 self.act = action.BUY
                 self.holding = True
                 self.holding_time = self.time_counter
@@ -65,13 +65,27 @@ class tcn_agent(agent_thread):
             return self.act
         else:
             pass
+        if self.time_counter - 1 == 776:
+            plt.ion()
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(111)
+
+            self.line1, = self.ax.plot(np.arange(len(self.hist_prediction)), self.hist_prediction, 'r-')
 
     def model(self):
         #build model around TCN
         self.built = True
         i = Input(batch_shape=(self.batch_size, self.moments-1, self.input_dim))
-        x1 = TCN(return_sequences=True)(i)
-        o = TCN(return_sequences=False)(x1)
+        x1 = TCN(return_sequences=True, dropout_rate=.0, kernel_size=2)(i)
+        x2 = Lambda(lambda z: backend.reverse(z, axes = -1))(i)
+        x2 = TCN(return_sequences=True, dropout_rate=.0, kernel_size=2)(i)
+        x = add([x1,x2])
+        x1 = TCN(return_sequences=True, dropout_rate=.0, kernel_size=2)(x)
+        x2 = Lambda(lambda z: backend.reverse(z, axes=-1))(x)
+        x2 = TCN(return_sequences=True, dropout_rate=.0, kernel_size=2)(x2)
+        x = add([x1, x2])
+        #x = TCN(return_sequences=True)(x)
+        o = concatenate([GlobalMaxPooling1D()(x), GlobalAveragePooling1D()(x)])
         o = Dense(4)(o)
         self.m = Model(inputs=i, outputs=o)
         self.m.compile(optimizer='adam', loss='mse') #optimizer and loss can be changed to what we want
@@ -84,7 +98,7 @@ class tcn_agent(agent_thread):
         input = np.array(input)
         input = np.atleast_2d(input)
         #Normalize data
-        input = self.normalization(input, 'lognormal')
+        input = self.normalization(input, 'costume')
         for i in range(input.shape[0] - moments+1):
             x_values = np.array(input[i:moments + i - lookahead])
             y_values = np.array(input[i+moments-lookahead:i+moments])
@@ -123,7 +137,41 @@ class tcn_agent(agent_thread):
                         inner_data.append(np.log(value / data[i-1][j]))
                 normalized_data.append(inner_data)
             return np.array(normalized_data)
-        return preprocessing.normalize(data)
+        elif mode == 'costume':
+            normalized_data = list()
+            for i, data_pt in enumerate(data):
+                inner_data = list()
+                for j, value in enumerate(data_pt):
+                    if (i == 0):
+                        if (j < 2):
+                            inner_data.append(np.log(value / value))
+                        else:
+                            inner_data.append(np.log(value/data_pt[0]))
+                    else:
+                        if (j < 2):
+                            inner_data.append(np.log(value / data[i - 1][j]))
+                        else:
+                            inner_data.append(np.log(value/data_pt[0]))
+                normalized_data.append(inner_data)
+            return np.array(normalized_data)
+        elif mode == "percentile":
+            normalized_data = list()
+            for i, data_pt in enumerate(data):
+                inner_data = list()
+                for j, value in enumerate(data_pt):
+                    if (i == 0):
+                        if (j < 1):
+                            inner_data.append(0)
+                        else:
+                            inner_data.append((value - data_pt[0])/data_pt[0])
+                    else:
+                        if (j < 1):
+                            inner_data.append((value - data[i-1][j+1]/ data[i - 1][j+1]))
+                        else:
+                            inner_data.append((value - data_pt[0])/data_pt[0])
+                normalized_data.append(inner_data)
+            return np.array(normalized_data)
+        return data
 
     def train(self, data_path):
         def read_data():
@@ -137,7 +185,8 @@ class tcn_agent(agent_thread):
         #x = np.array(x)
         #x = np.atleast_3d(x)
         #y = np.atleast_2d(y)
-        self.m.fit(x, y, epochs=10, validation_split=0.2)
+        self.m.fit(x, y, epochs=20, validation_split=0.1)
+        
     def run_model(self):
         inputs = self.market_history[-self.moments:]
         x, y = self.split_data(inputs, self.moments)
@@ -145,6 +194,7 @@ class tcn_agent(agent_thread):
         #x = np.atleast_3d(x)
         #y = np.atleast_2d(y)
         y_hat = self.m.predict(x)
+        self.hist_prediction.append((y_hat[0], y[0]))
         print("Predicting next price to be: ", y_hat[0])
         print("Real next price was: ", y[0])
         return y_hat[0]
